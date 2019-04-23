42287b030a754aae2371222b452adf382651edc7
yunus <yunus.karabulut1@gmail.com>
2019-04-23 14:42:11 +0300

    -----

---
+++ Query5.sql
@@ -0,0 +1,21 @@
+--List artists who have five or more albums that are at least 2500000 ms long (i.e., the total of
+--all the tracks on the album is at least 2500000 ms). Your output columns should be listed
+--as ArtistName and NumAlbums in that order
+
+
+select ar."Name",count(sub."AlbumId") as num_albums 
+from (
+	    
+      select "AlbumId", sum("Milliseconds") as album_length
+      from "Track"
+      group by 1
+      having sum("Milliseconds") >2500000
+      order by 2 desc
+      
+	  ) as sub
+join "Album" as al
+on al."AlbumId"=sub."AlbumId"
+join "Artist" as ar
+on ar."ArtistId"=al."ArtistId"
+group by 1
+order by 2 desc;
